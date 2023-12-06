1a293d804ec9170e01566a3d70e66024d796c262
Ahmed Adel Ahmed Ramadan <20912017100054@fci.zu.edu.eg>
2023-12-06 14:06:36 +0200

    Setup needed routes and controllers

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,13 @@
+# app/controllers/api/v1/categories_controller.rb
+
+module Api
+    module V1
+      class CategoriesController < ApplicationController
+        def index
+          @categories = Category.all
+          render json: @categories
+        end
+      end
+    end
+  end
+  
